2296e8812aa2690df124ab434973483b33ce5aa7
Anderson <loonander@gmail.com>
2023-09-07 16:34:03 -0300

    troca no sistema de editar nome do user

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,4 +1,4 @@
-
+// verificação de cadastro //
 let nomeCadastro = document.getElementById('name-cadastro')
 let senhaCadastro = document.getElementById('senha-cadastro')
 
@@ -6,36 +6,97 @@ let labelNome = document.getElementById('label-nome')
 let labelSenha = document.getElementById('label-senha')
 
 
-nomeCadastro.addEventListener('keyup', () =>{
-   if( nomeCadastro.value.length <= 4){
-      labelNome.setAttribute('style', 'color: red')
-      labelNome.innerHTML = '<strong>Nome *Insira no minimo 4 caracteres </strong>'
-      nomeCadastro.setAttribute('style','border-color: red')
-   }else{
-      labelNome.setAttribute('style','color: green')
-      labelNome.innerHTML = 'Nome'
-      nomeCadastro.setAttribute('style','border-color: greeb')
-   }
+nomeCadastro.addEventListener('keyup', () => {
+  if (nomeCadastro.value.length <= 4) {
+    labelNome.setAttribute('style', 'color: red')
+    labelNome.innerHTML = '<strong>Nome *Insira no minimo 4 caracteres </strong>'
+    nomeCadastro.setAttribute('style', 'border-color: red')
+  } else {
+    labelNome.setAttribute('style', 'color: green')
+    labelNome.innerHTML = 'Nome'
+    nomeCadastro.setAttribute('style', 'border-color: greeb')
+  }
 
 
 
 })
 
-senhaCadastro.addEventListener('keyup', () =>{
-   if( senhaCadastro.value.length <= 4){
-      labelSenha.setAttribute('style', 'color: red')
-      labelSenha.innerHTML = '<strong>Senha *Insira no minimo 8 caracteres </strong>'
-      senhaCadastro.setAttribute('style','border-color: red')
-   }else{
-      labelSenha.setAttribute('style','color: green')
-      labelSenha.innerHTML = 'Senha'
-      senhaCadastro.setAttribute('style','border-color: greeb')
-   }
+senhaCadastro.addEventListener('keyup', () => {
+  if (senhaCadastro.value.length < 8) {
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = '<strong>Senha *Insira no minimo 8 caracteres </strong>'
+    senhaCadastro.setAttribute('style', 'border-color: red')
+  } else {
+    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.innerHTML = 'Senha'
+    senhaCadastro.setAttribute('style', 'border-color: greeb')
+  }
 
 
 
 })
 
 
+// let labelNome = document.getElementById('label-nome')
+// let labelSenha = document.getElementById('label-senha')
 
-console.log(localStorage.getItem("name","senha"))
+
+// nomeCadastro.addEventListener('keyup', () =>{
+//    if( nomeCadastro.value.length <= 4){
+//       labelNome.setAttribute('style', 'color: red')
+//       labelNome.innerHTML = '<strong>Nome *Insira no minimo 4 caracteres </strong>'
+//       nomeCadastro.setAttribute('style','border-color: red')
+//    }else{
+//       labelNome.setAttribute('style','color: green')
+//       labelNome.innerHTML = 'Nome'
+//       nomeCadastro.setAttribute('style','border-color: greeb')
+//    }
+
+
+
+// })
+
+// senhaCadastro.addEventListener('keyup', () =>{
+//    if( senhaCadastro.value.length <= 4){
+//       labelSenha.setAttribute('style', 'color: red')
+//       labelSenha.innerHTML = '<strong>Senha *Insira no minimo 8 caracteres </strong>'
+//       senhaCadastro.setAttribute('style','border-color: red')
+//    }else{
+//       labelSenha.setAttribute('style','color: green')
+//       labelSenha.innerHTML = 'Senha'
+//       senhaCadastro.setAttribute('style','border-color: greeb')
+//    }
+
+
+
+// })
+
+
+
+// console.log(localStorage.getItem("name","senha"))
+
+
+
+
+
+
+// if(novoNome != usuarioAtual && verificaNome(novoNome)){
+//    if(novoNome.length >= 4 && novoNome.length <= 15 && novoSenha.length >= 8 && novoSenha.length <=15 ){
+//      users[index].nome = novoNome;
+//      users[index].senha = novoSenha;
+//      teste()
+     
+
+
+//    }else{
+//      alert("Preencha todos os campos seguindo as orientações corretamente.");
+//    }
+
+// }else if(novoNome = usuarioAtual){
+//    if(novoSenha.length >= 8 && novoSenha.length <=15 ){
+//    users[index].senha = novoSenha;
+//      teste()
+//    }
+//  }else{
+//    alert('o novo nome escrito já existe')
+//  }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,45 @@
 //  criando classe usuarios //
-class User{
-    constructor(nome,senha){
-        this.nome = nome
-        this.senha = senha
-    }
+class User {
+  constructor(nome, senha) {
+    this.nome = nome
+    this.senha = senha
+  }
 
 
 }
 
 const users = []
-             
-const btnCad = document.getElementById('cadastarOutro') 
+
+const btnCad = document.getElementById('cadastarOutro')
 
 const tabela = document.getElementById('tabelaUser')
 
 // função para criar os usuarios e empurrar eles para dentro do array
-function criaUser(nome,senha) {
+function criaUser(nome, senha) {
 
-        let newUser = new User(nome,senha)
-        users.push(newUser)
-        limparCampos()
-        
-    
-    
+  let newUser = new User(nome, senha)
+  users.push(newUser)
+  limparCampos()
+
+
+
 }
 // essa função retorna o que vai ser escrito dentro do html //
-function preencheTable(user,index){
-    return ` <table id="tabela">
+function preencheTable(user, index) {
+  return ` <table id="tabela">
     <tr>
       <th>Nome</th>
       <th>Senha</th>
       <th>ações</th>
     </tr>
     <tr>
-      <td>${user.nome}</td>
-      <td>${user.senha}</td>
+      <td>
+        <span id="nome-${index}">${user.nome}</span>
+        <input type="text" id="input-nome-${index}" maxlength="15" class="edit">
+      </td>
+      <td><span id="senha-${index}">${user.senha}</span>
+      <input type="password" id="input-senha-${index}" maxlength="15" class="edit">
+      </td>
       <td class="icons-table"> <img src="/assets/icons/pencil.svg" alt="lapis" class="icon-edit" onclick="editarUsuario(${index})"> <img src="/assets/icons/trash.svg" alt="lixeira" class="icon-edit" onclick="excluirUsuario(${index})"></td>
     </tr>
   </table> `
@@ -43,83 +48,147 @@ function preencheTable(user,index){
 }
 
 // essa função faz aparecer na tela o que esta dentro do array de usuarios // 
-function teste(){
-      tabela.innerHTML = ""
-      users.forEach((user,index) =>{
+function teste() {
+  tabela.innerHTML = ""
+  users.forEach((user, index) => {
 
-        tabela.innerHTML += preencheTable(user,index)
-      })
+    tabela.innerHTML += preencheTable(user, index)
+  })
 
 
 }
 // essa função edita o usuario de acordo com o index do botão clicado//
 
-function editarUsuario(index){
-      const novoNome = prompt('Digite a alteração no nome (minimo: 4 caracteres|máximo: 15 caracteres)')
-      const novoSenha = prompt('Digite a alteração na senha (minimo: 8 caracteres|máximo: 15 caracteres)')
+function editarUsuario(index) {
+  const nomeSpan = document.getElementById(`nome-${index}`);
+  const inputNome = document.getElementById(`input-nome-${index}`);
+  const senhaSpan = document.getElementById(`senha-${index}`);
+  const inputSenha = document.getElementById(`input-senha-${index}`);
 
-      if(novoNome.length >= 4 && novoNome.length <= 15 && novoSenha.length >= 8 && novoSenha.length <=15 ){
-        users[index].nome = novoNome;
-        users[index].senha = novoSenha;
-        teste()
-      }else{
-        alert("Preencha todos os campos seguindo as orientações corretamente.");
+  //ativa os campos de entrada editaveis e faz com o que já esteja escrito no span suma//
+  nomeSpan.style.display = 'none';
+  inputNome.style.display = 'inline-block';
+  senhaSpan.style.display = 'none';
+  inputSenha.style.display = 'inline-block';
+
+  inputNome.value = users[index].nome;
+  inputSenha.value = users[index].senha;
+
+  inputNome.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const novoNome = inputNome.value;
+      const novoSenha = inputSenha.value;
+
+      if (novoNome != users[index].nome && verificaNome(novoNome)) {
+        alert("Este nome já está em uso por outro usuário.");
+        return;
       }
+
+      if (novoSenha.length < 8) {
+        alert("A senha deve conter pelo menos 8 caracteres.");
+        return;
+      }
+
+      if (novoNome.length < 4) {
+        alert("O nome deve conter pelo menos 4 caracteres.");
+        return;
+      }
+
+
+      users[index].nome = novoNome;
+      users[index].senha = novoSenha;
+
+      // Atualizar a exibição da tabela
+      teste();
+    }
+  });
+
+  inputSenha.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const novoNome = inputNome.value;
+      const novoSenha = inputSenha.value;
+
+      if (novoNome != users[index].nome && verificaNome(novoNome)) {
+        alert("Este nome já está em uso por outro usuário.");
+        return;
+      }
+
+      if (novoSenha.length < 8) {
+        alert("A senha deve conter pelo menos 8 caracteres.");
+        return;
+      }
+
+      if (novoNome.length < 4) {
+        alert("O nome deve conter pelo menos 4 caracteres.");
+        return;
+      }
+
+      users[index].nome = novoNome;
+      users[index].senha = novoSenha;
+
+      // Atualizar a exibição da tabela
+      teste();
+    }
+  })
+
+
+
 }
 
 // botão que leva para tela de cadastro //
-btnCad.addEventListener('click', ()=>{
-    location.href = "#tela-cadastro"
-    // tabela.innerHTML = ""
+btnCad.addEventListener('click', () => {
+  location.href = "#tela-cadastro"
+  // tabela.innerHTML = ""
 
 })
 
-    function limparCampos(){
-      document.getElementById('name-cadastro').value= ""
-      document.getElementById('senha-cadastro').value = ""
-    }
-
-    function excluirUsuario(index){
-      if (confirm("Tem certeza que deseja excluir este usuario?")) {
-        users.splice(index, 1);
-        teste();
-    }
+function limparCampos() {
+  document.getElementById('name-cadastro').value = ""
+  document.getElementById('senha-cadastro').value = ""
 }
-    
+
+function excluirUsuario(index) {
+  if (confirm("Tem certeza que deseja excluir este usuario?")) {
+    users.splice(index, 1);
+    teste();
+  }
+}
+
+function verificaNome(nome) {
+  return users.some(function (usuario) {
+    return usuario.nome === nome;
+  });
+}
+
+
+
+
 // botao que cadastra usuario novo e ativa as outras funções para apresentar tudo na tela //
 const btn = document.getElementById('btn-cadastro')
 
-btn.addEventListener('click', ()=>{
-  let nome =  document.getElementById('name-cadastro').value
+btn.addEventListener('click', () => {
+  let nome = document.getElementById('name-cadastro').value
   let senha = document.getElementById('senha-cadastro').value
 
-    if(nome && senha){
+  if (!verificaNome(nome)) {
+    if (senha.length >= 8 && nome.length >= 4) {
       location.href = "#edit-tela";
-      criaUser(nome,senha)
-      teste()
-
-      console.log(nome,senha)
-    console.log(users)
-      limparCampos()
-    
-    }else{
-
-      console.log(nome,senha)
-      console.log(users)
+      criaUser(nome, senha);
+      teste();
+      console.log(nome, senha);
+      console.log(users);
+      limparCampos();
+    } else {
+      console.log(nome, senha);
+      console.log(users);
     }
-   
-
-
-   
+  } else {
+    alert("Nome já utilizado.");
+    console.log(nome, senha);
+    console.log(users);
+  }
 })
 
-// for (let i = 0; i < users.length; i++) {
-//     const user = users[i];
-    
-  
-//     tabela.innerHTML += preencheTable(user)
-    
 
 
-    
-// }
+
